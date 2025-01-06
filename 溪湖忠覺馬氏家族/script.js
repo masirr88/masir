6247fc1f0ce7b9ed3c2d3ad15cfe387aa09e53f7
masirr88 <43272436+masirr88@users.noreply.github.com>
2025-01-06 10:01:11 +0800

    Delete 溪湖忠覺馬氏家族 directory

--- 溪湖忠覺馬氏家族/script.js
+++
@@ -1,7 +0,0 @@
-const toggleSidebar = ()=>{
-    const sidebar = document.querySelector('.sidebar');
-    const openMenu = document.querySelector('.open-menu');
-
-    sidebar.classList.toggle('active');
-    openMenu.classList.toggle('hidden');
-}
